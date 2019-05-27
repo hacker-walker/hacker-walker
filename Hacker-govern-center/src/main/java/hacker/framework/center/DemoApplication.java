187eb67fd16928b185418e3e0c47538d38254fe0
liuqi <a18627830855@163.com>
2019-05-27 18:54:59 +0800

    jinxianianhua——》添加各种牛逼的工具类------作者：LuKe  :fire:

--- Hacker-govern-center/src/main/java/hacker/framework/center/DemoApplication.java
+++ Hacker-govern-center/src/main/java/hacker/framework/center/DemoApplication.java
@@ -1,4 +1,4 @@
-package hacker.framework.center;
+package hacker.framework.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
